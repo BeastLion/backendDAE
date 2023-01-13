ed75da90ebad1ad463cdac994c87432843c5cc3b
tiago479 <76876635+tiago479@users.noreply.github.com>
2023-01-13 21:47:47 +0000

    a

--- seguradora/src/main/java/pt/ipleiria/estg/dei/ei/dae/seguradora/ejbs/ExpertBean.java
+++ seguradora/src/main/java/pt/ipleiria/estg/dei/ei/dae/seguradora/ejbs/ExpertBean.java
@@ -2,8 +2,10 @@ package pt.ipleiria.estg.dei.ei.dae.seguradora.ejbs;
 
 import pt.ipleiria.estg.dei.ei.dae.seguradora.entities.Insurer.InsurerOwner;
 import pt.ipleiria.estg.dei.ei.dae.seguradora.entities.Users.Expert;
+import pt.ipleiria.estg.dei.ei.dae.seguradora.security.Hasher;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -12,8 +14,11 @@ public class ExpertBean {
     @PersistenceContext
     private EntityManager em;
 
+    @Inject // import javax.inject.Inject;
+    private Hasher hasher;
+
     public void create(String username, String name, String lastName, String phoneNumber, String password, String email) {
-        var expert = new Expert(username, name, lastName, phoneNumber, password, email);
+        var expert = new Expert(username, name, lastName, phoneNumber, hasher.hash(password), email);
         em.persist(expert);
     }
 
--- seguradora/src/main/java/pt/ipleiria/estg/dei/ei/dae/seguradora/ejbs/TechnicianBean.java
+++ seguradora/src/main/java/pt/ipleiria/estg/dei/ei/dae/seguradora/ejbs/TechnicianBean.java
@@ -1,8 +1,10 @@
 package pt.ipleiria.estg.dei.ei.dae.seguradora.ejbs;
 
 import pt.ipleiria.estg.dei.ei.dae.seguradora.entities.Users.Technician;
+import pt.ipleiria.estg.dei.ei.dae.seguradora.security.Hasher;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -11,8 +13,11 @@ public class TechnicianBean {
     @PersistenceContext
     private EntityManager em;
 
+    @Inject // import javax.inject.Inject;
+    private Hasher hasher;
+
     public void create(String username, String name, String lastName, String phoneNumber, String password, String email) {
-        var technician = new Technician(username, name, lastName, phoneNumber, password, email);
+        var technician = new Technician(username, name, lastName, phoneNumber, hasher.hash(password), email);
         em.persist(technician);
     }
 
