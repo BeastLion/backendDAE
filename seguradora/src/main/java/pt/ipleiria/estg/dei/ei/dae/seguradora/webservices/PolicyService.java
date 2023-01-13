dc3c8967a3d5281277a47e1a54f223702f58c657
Bernardo Bernardino <bernardoluisbernardino@gmail.com>
2023-01-13 23:34:15 +0000

    asdasd

---
+++ seguradora/src/main/java/pt/ipleiria/estg/dei/ei/dae/seguradora/webservices/PolicyService.java
@@ -0,0 +1,47 @@
+package pt.ipleiria.estg.dei.ei.dae.seguradora.webservices;
+
+import pt.ipleiria.estg.dei.ei.dae.seguradora.DTOs.PolicyDTO;
+import pt.ipleiria.estg.dei.ei.dae.seguradora.ejbs.ClientBean;
+import pt.ipleiria.estg.dei.ei.dae.seguradora.ejbs.PolicyBean;
+import pt.ipleiria.estg.dei.ei.dae.seguradora.ejbs.UserBean;
+import pt.ipleiria.estg.dei.ei.dae.seguradora.security.Authenticated;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import java.util.List;
+
+@Path("policy")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
+public class PolicyService {
+    @EJB
+    private UserBean userBean;
+    @EJB
+    private ClientBean clientBean;
+    @EJB
+    private PolicyBean policyBean;
+    @Context
+    private SecurityContext securityContext;
+
+    @GET
+    @Authenticated
+    @Path("/client")
+    public Response getPolicyByAuthenticated() {
+        var username = securityContext.getUserPrincipal().getName();
+        List<PolicyDTO> policyList = PolicyDTO.toDTOs(policyBean.getPolicyByUsername(username));
+
+        if (policyList.isEmpty()) {
+            return Response.noContent().build();
+        }
+
+        return Response.ok(policyList).build();
+
+    }
+}
